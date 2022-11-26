9576c37247c94052d576c3567ea30bbc4176cd52
Magic Coder <magicoder20@gmail.com>
2022-11-26 11:15:35 -0800

    Add concurrent test runner (#7)

---
+++ src/tester/main.go
@@ -0,0 +1,109 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"strconv"
+	"time"
+)
+
+func main() {
+	args := os.Args[1:]
+	if len(args) < 5 {
+		fmt.Println("Usage: go run runner/main.go [testPattern] [testSrcDir] [totalTimes] [logOutputDir] [numParallelWorkers]")
+		fmt.Println("Eg. go run runner/main.go TestFigure8Unreliable2C ./raft/... 2000 ./logs/TestFigure8Unreliable2C-2 10")
+		return
+	}
+
+	testPattern := args[0]
+	testSrcDir := args[1]
+	totalTimesArg, err := strconv.ParseInt(args[2], 10, 64)
+	if err != nil {
+		panic(err)
+	}
+
+	logOutputDir := args[3]
+	numWorkersArg, err := strconv.ParseInt(args[4], 10, 64)
+	if err != nil {
+		panic(err)
+	}
+
+	numWorkers := int(numWorkersArg)
+	totalTimes := int(totalTimesArg)
+
+	os.RemoveAll(logOutputDir)
+	err = os.MkdirAll(logOutputDir, 0700)
+	if err != nil {
+		panic(err)
+	}
+
+	runTestCh := make(chan int)
+	finishRunCh := make(chan bool)
+
+	startAt := time.Now()
+	for worker := 0; worker < numWorkers; worker++ {
+		go func() {
+			for testIndex := range runTestCh {
+				func(testIndex int) {
+					path := fmt.Sprintf("%v/%v.log", logOutputDir, testIndex)
+					outputFile, err := os.Create(path)
+					if err != nil {
+						fmt.Println(err)
+						finishRunCh <- false
+						return
+					}
+
+					defer outputFile.Close()
+
+					cmd := exec.Command("go", "test", testSrcDir, "--race", "-run", testPattern, "--count=1", "-v")
+					cmd.Stdout = outputFile
+					cmd.Stderr = outputFile
+					err = cmd.Start()
+					if err != nil {
+						fmt.Println(err)
+						finishRunCh <- false
+						return
+					}
+
+					err = cmd.Wait()
+					if err != nil {
+						fmt.Println(err)
+						finishRunCh <- false
+						return
+					}
+
+					finishRunCh <- true
+				}(testIndex)
+			}
+		}()
+	}
+
+	go func() {
+		for runIndex := 0; runIndex < totalTimes; runIndex++ {
+			runTestCh <- runIndex
+		}
+
+		close(runTestCh)
+	}()
+
+	totalFinishedTests := 0
+	passedTests := 0
+	failedTests := 0
+	for testResult := range finishRunCh {
+		totalFinishedTests++
+		fmt.Printf("Total finished tests:%v, failed tests:%v, total time:%v\n", totalFinishedTests, failedTests, time.Now().Sub(startAt))
+
+		if testResult {
+			passedTests++
+		} else {
+			failedTests++
+		}
+
+		if totalFinishedTests == totalTimes {
+			close(finishRunCh)
+		}
+	}
+
+	fmt.Printf("Finished running all tests: total=%v passed=%v failed=%v\n", totalFinishedTests, passedTests, failedTests)
+}
